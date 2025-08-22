d363295ccf0ee9157c4fee42f89a6f03e64716e6
skdod2453 <skdod2453@gmail.com>
2025-08-22 14:45:19 +0900

    Feat: 코스 리스트의 코스 이름 api 연결

--- src/routes/pages/CourseBoard.tsx
+++ src/routes/pages/CourseBoard.tsx
@@ -1,20 +1,41 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import CourseList, { type CourseData } from "../../components/CourseList";
 import CoursePlaces from "../../components/CoursePlaces";
 import CourseDetail from "../../components/CourseDetail";
+import axios from "axios";
 
 export default function CourseBoard() {
   const [selectedCourse, setSelectedCourse] = useState<CourseData | null>(null);
+  const [courseList, setCourselist] = useState<CourseData[]>([]);
 
-  const courseList: CourseData[] = [
-    {
-      title: "대전대 IoT의 일상",
-      time: "07:30 ~ 08:00",
-      duration: "30분",
-      tags: "#대학생 #대전 #일상 #IoT",
-      imgSrc: "src/images/school.png",
-    },
-  ];
+  useEffect(() => {
+    (async () => {
+      try {
+        const rawCourses = await axios.get("/api/courses");
+        console.log("API응답 데이터 : ", rawCourses.data);
+        const convertedCourses: CourseData[] = rawCourses.data.map((course: any) => ({
+        title: course.course_name,
+        time: "07:30 ~ 08:00",       // 임의 데이터
+        duration: "30분",             // 임의 데이터
+        tags: "#대학생 #대전 #일상 #IoT", // 임의 데이터
+        imgSrc: "src/images/school.png", // 임의 데이터
+      }));
+      setCourselist(convertedCourses);
+      } catch (error) {
+        console.error("API 호출 실패 : ", error);
+      }
+    })();
+  }, []);
+
+  // const courseList: CourseData[] = [
+  //   {
+  //     title: "대전대 IoT의 일상",
+  //     time: "07:30 ~ 08:00",
+  //     duration: "30분",
+  //     tags: "#대학생 #대전 #일상 #IoT",
+  //     imgSrc: "src/images/school.png",
+  //   },
+  // ];
 
   return (
     <div className="p-15 bg-base-100 min-h-screen flex justify-center">
